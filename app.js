b9d78a4cadf464698ed2b25c7026e952ba35af4a
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-19 19:56:19 +0900

    P.85 学習完了
    関数を切り分けた。

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
-var server = http.createServer((request, response) => {
+var server = http.createServer(getFromClient);
+
+// createServerの処理
+function getFromClient(req, res) {
+  request = req;
+  response = res;
   fs.readFile('./index.html', 'UTF-8', (error, data) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
     response.end();
   });
-});
+}
 
 server.listen(3000);
 console.log('Server start!');
