c0e6f83454d2d12620604be85eadc414e13828ae
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-19 20:44:47 +0900

    P.99まで学習完了
    EJSのインストール
    EJSのサンプルを追加

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
 
-var request;
-var response;
+const index_page = fs.readFileSync('./index.ejs', 'UTF-8');
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server start!');
 
 // createServerの処理
-function getFromClient(req, res) {
-  request = req;
-  response = res;
-  fs.readFile('./index.html', 'UTF-8', writeToResponse);
-}
-
-// readFile完了後の処理
-function writeToResponse(error, data) {
-  var content = data.replace(/dummy_title/g, 'タイトルです').replace(/dummy_content/g, 'これがコンテンツです。')
+function getFromClient(request, response) {
+  var content = ejs.render(index_page);
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
