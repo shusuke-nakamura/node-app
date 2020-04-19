1c60e2ef58ba2daf51b4d23e9c202a9371db0493
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-19 20:02:31 +0900

    P.86学習完了
    コールバック関数を分けて記述

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 
+var request;
+var response;
+
 var server = http.createServer(getFromClient);
+server.listen(3000);
+console.log('Server start!');
 
 // createServerの処理
 function getFromClient(req, res) {
   request = req;
   response = res;
-  fs.readFile('./index.html', 'UTF-8', (error, data) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
-  });
+  fs.readFile('./index.html', 'UTF-8', writeToResponse);
 }
 
-server.listen(3000);
-console.log('Server start!');
+// readFile完了後の処理
+function writeToResponse(error, data) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(data);
+  response.end();
+}
