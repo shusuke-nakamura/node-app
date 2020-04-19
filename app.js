20ec8667febe8a19b2aeaed0c803f4f88c20a9f4
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-19 20:11:14 +0900

    P.91 学習完了
    コンテンツを動的に切り替える。

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ function getFromClient(req, res) {
 
 // readFile完了後の処理
 function writeToResponse(error, data) {
+  var content = data.replace(/dummy_title/g, 'タイトルです').replace(/dummy_content/g, 'これがコンテンツです。')
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(data);
+  response.write(content);
   response.end();
 }
