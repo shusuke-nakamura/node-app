52e431b4395827abd4e9bc8bead5ae0435f4f7aa
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-19 19:39:50 +0900

    P.78まで学習完了

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 var server = http.createServer((request, response) => {
-  response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<!DOCTYPE html><html lang="ja">');
+  response.write('<head><meta charset="utf-8">');
+  response.write('<title>Hello</title></head>');
+  response.write('<body><h1>Hello Node.js!</h1>');
+  response.write('<p>This is Node.js sample page.</p>');
+  response.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf8');
+  response.write('</body></html>');
+  response.end();
 });
 
 server.listen(3000);
+console.log('Server start!');
