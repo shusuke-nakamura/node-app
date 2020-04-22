d9efe2c2b2e3ed9eba1d4edbb8c9c7285ed80fa5
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-22 23:16:08 +0900

    P.112まで学習完了

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
-const index_page = fs.readFileSync('./index.ejs', 'UTF-8');
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
@@ -10,11 +12,26 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
-  var content = ejs.render(index_page, {
-    title: "Index",
-    content: "これはテンプレートを使用したサンプルページです。",
-  });
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(content);
-  response.end();
+  var url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+    case '/':
+      var content = ejs.render(index_page, {
+        title: "Index",
+        content: "これはテンプレートを使用したサンプルページです。",
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+      break;
+    case '/style.css':
+      response.writeHead(200, { 'Content-Type': 'text/css' });
+      response.write(style_css);
+      response.end();
+      break;
+    default:
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.write('no page...');
+      response.end();
+      break;
+  }
 }
