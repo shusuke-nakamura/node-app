9850b1376e834887ce85253f216c3410fc82c968
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-23 17:40:20 +0900

    P.142  まで学習完了

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ function response_index(request, response) {
     title: "Index",
     content: msg,
     data: data,
+    filename: 'data_item'
   });
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
