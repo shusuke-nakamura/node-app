4d04c0ee1357aca38640d6fd5b04b16c6a8f6463
FSI中村 修介 <nakshusu@fsi.co.jp>
2020-04-24 15:10:50 +0900

    P.156 学習完了

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ function response_index(request, response) {
 
     request.on('end', () => {
       data = qs.parse(body);
+      // クッキーの保存
+      setCookie('msg', data.msg, response);
       write_index(request, response);
     });
   } else {
@@ -65,14 +67,34 @@ function response_index(request, response) {
 
 function write_index(request, response) {
   var msg = "※伝言を表示します。";
+  var cookie_data = getCookie('msg', request);
   var content = ejs.render(index_page, {
     title: "Index",
     content: msg,
     data: data,
+    cookie_data: cookie_data,
   });
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write(content);
   response.end();
+}
+
+// クッキーの値を設定
+function setCookie(key, value, response) {
+  var cookie = escape(value);
+  response.setHeader('Set-Cookie', [key + '='  + cookie]);
+}
+
+function getCookie(key, request) {
+  var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+  var data = cookie_data.split(';');
+  for(var i in data) {
+    if (data[i].trim().startsWith(key + '=')) {
+      var result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return '';
 }
 
 function response_other(request, response) {
